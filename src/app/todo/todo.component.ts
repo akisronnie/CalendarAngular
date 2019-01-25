a0454816da99baa4a9b434eb9bc6c172e41a3470
Alexandr Kyslynskyi <alexandr.kyslynskyi@daxx.com>
2019-01-25 15:40:54 +0200

    fixed

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,54 +1,58 @@
-import { Component, Output, EventEmitter, Input } from '@angular/core';
+import { Component, Output, EventEmitter, Input, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
   styleUrls: ['./todo.component.css']
 })
+
 export class TodoComponent {
-  @Input() public selectedDate: TDate;
+  @Input() public selectedDateToShow: TDate;
   @Input() public selectedNotes: TNote[];
-  @Input() public isShowEditField: boolean;
-  @Output() public changedInput: EventEmitter<TNote[]> = new EventEmitter;
+  @Input() public isNewNote: boolean = true;
+  @Output() public changeSelectedNotes: EventEmitter<TNote[]> = new EventEmitter;
 
+  @ViewChild('inputForm') public inputForm: ElementRef;
 
-  public newTask: string;
-  public position: number = 0;
+  public positionForId: number = 0;
   public editField: string;
-  public editNote: TNote;
+  public editOneNote: TNote;
 
-  public changeInput(): void {
 
-    this.selectedNotes.forEach((note: { id: number }) => {
-      if (note.id > this.position) {
-        this.position = note.id + 1;
-      }
-    });
+  public changeRedact(): void {
+    if (this.isNewNote) {
+      this.selectedNotes.forEach((note: { id: number }) => {
+        if (note.id > this.positionForId) {
+          this.positionForId = note.id + 1;
+        }
+      });
 
-    this.selectedNotes.push({ text: this.newTask, success: false, id: this.position });
-    this.changedInput.emit(this.selectedNotes);
-    this.newTask = '';
+      this.selectedNotes.push({ text: this.editField, success: false, id: this.positionForId });
+
+    } else {
+      this.editOneNote.text = this.editField;
+      this.isNewNote = true;
+    }
+
+    this.changeSelectedNotes.emit(this.selectedNotes);
+    this.editField = '';
   }
 
   public changeNote(note: TNote): void {
-    this.isShowEditField = false;
     this.editField = note.text;
-    this.editNote = note;
-  }
-
-  public changeRedact(): void {
-    this.editNote.text = this.editField;
-    this.isShowEditField = true;
+    this.editOneNote = note;
+    this.isNewNote = false;
+    this.inputForm.nativeElement.focus();
   }
 
   public deleteElem(deletedElement: TNote): void {
     this.selectedNotes = this.selectedNotes.filter((list: TNote) => list !== deletedElement);
-    this.changedInput.emit(this.selectedNotes);
+    this.changeSelectedNotes.emit(this.selectedNotes);
   }
 
   public checkInput(key: { success: boolean }): void {
     key.success = !key.success;
-    this.changedInput.emit(this.selectedNotes);
+    this.changeSelectedNotes.emit(this.selectedNotes);
   }
 }
 
